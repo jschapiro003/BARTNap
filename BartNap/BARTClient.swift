d61f377e7246cf34c6ffa48616751a2a10e1f0ed
Jonathan Schapiro <jschapir@gmail.com>
2014-10-19 12:25:22 -0700

    added initial BartClient

---
+++ BartNap/BARTClient.swift
@@ -0,0 +1,35 @@
+//
+//  BARTClient.swift
+//  BartNap
+//
+//  Created by Jonathan Schapiro on 10/18/14.
+//  Copyright (c) 2014 Jonathan Schapiro. All rights reserved.
+//
+
+import UIKit
+
+class BARTClient: NSObject {
+    /*
+ 
+    let baseURL: String = "http://api.bart.gov/api/sched.aspx?"
+    let key: String =  "QAL4-UP9T-IIAQ-DT35"
+    var url =  NSURL(string: "http://api.bart.gov/api/sched.aspx?cmd=arrive&orig=ASHB&dest=CIVC&date=now&key=QAL4-UP9T-IIAQ-DT35")
+    var xmlParser = NSXMLParser()
+
+    class var sharedInstance: BARTClient {
+        struct Static{
+            static let instance =  BARTClient()
+        }
+        return Static.instance
+    }
+    
+    func scheduleInfo()->NSXMLParser{
+       println("hello world")
+      xmlParser = NSXMLParser(contentsOfURL: url)!
+        xmlParser.parse()
+    
+        return xmlParser
+    }
+    
+    */
+}
--- BartNap/ViewController.swift
+++ BartNap/ViewController.swift
@@ -1,25 +1,54 @@
+
 //
 //  ViewController.swift
-//  BartNap
-//
-//  Created by Jonathan Schapiro on 10/11/14.
-//  Copyright (c) 2014 Jonathan Schapiro. All rights reserved.
+//  SwiftXMLParsingDemo
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,NSXMLParserDelegate {
+    
+    var strXMLData:String = ""
+    var currentElement:String = ""
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var url:String="http://api.androidhive.info/pizza/?format=xml"
+        var urlToSend = NSURL(string: "http://api.bart.gov/api/sched.aspx?cmd=arrive&orig=ASHB&dest=CIVC&date=now&key=QALV-U3SB-I56Q-DT35&b=2&a=2&l=1")
+        // Parse the XML
+        var parser:NSXMLParser = NSXMLParser(contentsOfURL: urlToSend)!
+        parser.delegate = self
+        
+        var success:Bool = parser.parse()
+        
+        if success {
+            println("parse success!")
+         
+        } else {
+            println("parse failure!")
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func parser(parser: NSXMLParser!, didStartElement elementName: String!, namespaceURI: String!, qualifiedName qName: String!, attributes attributeDict: NSDictionary!) {
+        //println("Element's name is \(elementName)")
+        //println("Element's attributes are \(attributeDict)")
+        if (elementName == "trip" || elementName == "leg"){
+                println("trip: \(attributeDict)")
+            
+        }
+        
+    }
 
-
+    func parser(parser: NSXMLParser!, parseErrorOccurred parseError: NSError!) {
+        NSLog("failure error: %@", parseError)
+    }
 }
+
 
