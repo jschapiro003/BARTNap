cd71a8138b65068c54fd556cdb811f0ec66cc778
Jonathan Schapiro <jschapir@gmail.com>
2014-10-26 13:00:19 -0700

    "local commit, working code"

---
+++ BartNap/EstimatedTimeDeparture.swift
@@ -0,0 +1,13 @@
+//
+//  EstimatedTimeDeparture.swift
+//  BartNap
+//
+//  Created by Jonathan Schapiro on 10/26/14.
+//  Copyright (c) 2014 Jonathan Schapiro. All rights reserved.
+//
+
+import UIKit
+
+class EstimatedTimeDeparture: NSObject {
+   
+}
