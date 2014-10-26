8ce6314bca851cddd15078281c9325c31d5bde5d
Isabel Yepes <isabel.yepes@silveryfrog.com>
2014-10-26 14:49:21 -0700

    - Parser version 0

--- BartNap/ViewController.swift
+++ BartNap/ViewController.swift
@@ -7,7 +7,14 @@ import UIKit
 
 class ViewController: UIViewController, NSXMLParserDelegate {
     
-    let parser = NSXMLParser(contentsOfURL: NSURL(string:"http://api.bart.gov/api/stn.aspx?cmd=stns&key=QALV-U3SB-I56Q-DT35" ))
+    var strXMLData:String = ""
+    var currentElement:String = ""
+    var passData:Bool=false
+    var passName:Bool=false
+    
+    
+    
+    let parser:NSXMLParser? = NSXMLParser(contentsOfURL: NSURL(string:"http://api.bart.gov/api/stn.aspx?cmd=stns&key=QALV-U3SB-I56Q-DT35" ))
     var parseSuccess:Bool = true
     var stations = Array<Station>()
     
@@ -16,49 +23,68 @@ class ViewController: UIViewController, NSXMLParserDelegate {
     var stationAbbreviation:Bool?
     var stationLatitude:Bool?
     var stationLongitude:Bool?
- 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
         
-        
-        // we will allow users to enter there origin and destination based on a list of stations 
+        // we will allow users to enter there origin and destination based on a list of stations
         //there answers will give the requirements for a call to the schedule info API
         //we will pass these values along next view controller, where timer calculations will be done
         
         
+        /*var url:String="http://api.bart.gov/api/route.aspx?cmd=routes&key=MW9S-E7SL-26DU-VV8V"*/
+        var url:String="http://api.bart.gov/api/stn.aspx?cmd=stns&key=QALV-U3SB-I56Q-DT35"
+        var urlToSend: NSURL = NSURL(string: url)
+        // Parse the XML
+        var parser = NSXMLParser(contentsOfURL: urlToSend)
+        parser.delegate = self
         
-         var xmlParser = parser
-
+        var success:Bool = parser.parse()
+        
+        if success {
+            println("parse success!")
+            
+            println(strXMLData)
+            
+            //lblNameData.text=strXMLData
+            
+        } else {
+            println("parse failure!")
+        }
+        
+        
+        /*if let xmlParser:NSXMLParser = self.parser? {
+            
             xmlParser.delegate = self
-
-                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), {
+            
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), {
                 
-                    self.parseSuccess = xmlParser.parse()
-                    for station in self.stations{
-                        if let sn = station.name?{
-                            println("station name:\(sn)")
-                        }
-                        if let sa = station.abbreviation?{
-                            println("station abbreviation \(sa)")
-                        }
-
+                
+                self.parseSuccess = xmlParser.parse()
+                for station in self.stations{
+                    if let sn = station.name?{
+                        println("station name:\(sn)")
                     }
+                    if let sa = station.abbreviation?{
+                        println("station abbreviation \(sa)")
+                    }
+                    
+                }
                 
-                })
+            })
+        }
+        println("no longer in the background")*/
         
-        println("no longer in the background")
         
-       
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-   
     
-    func parser(parser: NSXMLParser!, didStartElement elementName: String!, namespaceURI: String!, qualifiedName qName: String!, attributes attributeDict: NSDictionary!) {
+    
+/*    func parser(parser: NSXMLParser!, didStartElement elementName: String!, namespaceURI: String!, qualifiedName qName: String!, attributes attributeDict: NSDictionary!) {
         //check to see if the staion name element exists
         self.stationName = (elementName == "name")
         
@@ -68,7 +94,7 @@ class ViewController: UIViewController, NSXMLParserDelegate {
         //check to see if gtfs latitude exists
         self.stationLatitude = (elementName == "gtfs_latitude")
         
-        //check to see if gtfs longitude exists 
+        //check to see if gtfs longitude exists
         self.stationLongitude = (elementName == "gtfs_longitude")
         
         
@@ -112,7 +138,7 @@ class ViewController: UIViewController, NSXMLParserDelegate {
                 currentLongitudeName = string
             }
         }
-
+        
         //create new station and populate it
         
         var station:Station = Station(name: currentStationName, abbreviation: currentAbbreviationName, latitude: currentLatitudeName, longitude: currentLongitudeName)
@@ -121,9 +147,44 @@ class ViewController: UIViewController, NSXMLParserDelegate {
         stations.append(station)
         
         
-    }
+    }*/
     
     func parser(parser: NSXMLParser!, parseErrorOccurred parseError: NSError!) {
         NSLog("failure error: %@", parseError)
     }
+    
+    
+    func parser(parser: NSXMLParser!,didStartElement elementName: String!, namespaceURI: String!, qualifiedName : String!, attributes attributeDict: NSDictionary!) {
+        currentElement=elementName;
+        if(elementName=="station" || elementName=="name" || elementName=="abbr" || elementName=="gtfs_latitude" || elementName=="gtfs_longitude")
+        {
+            if(elementName=="station"){
+                passName=true;
+            }
+            passData=true;
+        }
+    }
+    
+    func parser(parser: NSXMLParser!, didEndElement elementName: String!, namespaceURI: String!, qualifiedName qName: String!) {
+        currentElement="";
+        if(elementName=="station" || elementName=="name" || elementName=="abbr" || elementName=="gtfs_latitude" || elementName=="gtfs_longitude")
+        {
+            if(elementName=="station"){
+                passName=false;
+            }
+            passData=false;
+        }
+    }
+    
+    func parser(parser: NSXMLParser!, foundCharacters string: String!) {
+        if(passName){
+            strXMLData=strXMLData+"\n\n"+string
+        }
+        
+        if(passData)
+        {
+            println(string)
+        }
+    }
+    
 }
